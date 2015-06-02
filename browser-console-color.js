a54ab1204db31e1a6b7e169c3ade4f354770d321
eltoncezar <eltoncezar@gmail.com>
2015-06-02 15:46:11 -0300

    Revert "Revert "Initial commit""
    This reverts commit c58e06f1ee70f0edac90856bff785db5b6f131ad.

---
+++ browser-console-color.js
@@ -0,0 +1,28 @@
+console.color = function (text, color, background) {
+    var styles = [
+        'color:' + color,
+        'background-color:' + background,
+        'padding: 0px 8px;'
+    ].join(';');
+    console.log('%c' + text, styles);
+};
+
+var colors = {
+    navy: '#001f3f',
+    blue: '#0074d9',
+    aqua: '#7fdbff',
+    teal: '#39cccc',
+    olive: '#3d9970',
+    green: '#2ecc40',
+    lime: '#01ff70',
+    yellow: '#ffdc00',
+    orange: '#ff851b',
+    red: '#ff4136',
+    fuchsia: '#f012be',
+    purple: '#b10dc9',
+    maroon: '#85144b',
+    white: '#ffffff',
+    gray: '#aaaaaa',
+    silver: '#dddddd',
+    black: '#111111'
+};
